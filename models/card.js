c1f8296bc38994540e6d11674d020f2eab64f8c6
Terence Dumas <watsutx41@gmail.com>
2021-06-08 00:03:34 -0500

    better frontend on the create page

--- models/card.js
+++ models/card.js
@@ -1,37 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const cardSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    youtube: {
-        type: String
-    },
-    twitch: {
-        type: String
-    },
-    facebook: {
-        type: String
-    },
-    twitter: {
-        type: String
-    },
-    tiktok: {
-        type: String
-    },
-    instagram: {
-        type: String
-    },
-    linkedin: {
-        type: String
-    },
-    github: {
-        type: String
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  youtube: {
+    type: String,
+  },
+  twitch: {
+    type: String,
+  },
+  facebook: {
+    type: String,
+  },
+  twitter: {
+    type: String,
+  },
+  tiktok: {
+    type: String,
+  },
+  instagram: {
+    type: String,
+  },
+  linkedin: {
+    type: String,
+  },
+  github: {
+    type: String,
+  },
+});
 
 module.exports = mongoose.model("Card", cardSchema);
